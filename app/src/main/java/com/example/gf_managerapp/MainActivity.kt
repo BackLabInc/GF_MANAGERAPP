055224d459979d4d6949e8e60eb7042318c0dac7
LuisMarquez95 <120360961+BackLabInc@users.noreply.github.com>
2023-03-20 19:56:59 -0600

    SE AGREGAN PERMISOS AL MANIFEST, GRADLE CONFIG.

--- app/src/main/java/com/example/gf_managerapp/MainActivity.kt
+++ app/src/main/java/com/example/gf_managerapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.gf_managerapp
 
+import android.app.WallpaperManager
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gf_managerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*REVISAR LA SOCLICITUD DE PERMISOS*/
+
+        var wallpaperManager: WallpaperManager = WallpaperManager.getInstance(this);
+        var wallpaperDrawable: Drawable = wallpaperManager.getDrawable()
+
+        binding.wallpaperUser.setImageDrawable(wallpaperDrawable)
     }
 }
